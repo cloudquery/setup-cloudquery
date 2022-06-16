b848e465c186221aad147f96af66adaffc019a9e
erezrokah <erezrokah@users.noreply.github.com>
2022-06-16 17:34:27 +0300

    feat: Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# setup-cloudquery
+
+A GitHub action to download the CloudQuery CLI so it can be used in other steps.
+
+## Prerequisites
+
+* We only support Linux or MacOS runners. Contributions welcomed to add Windows support 🪟
+* A CloudQuery configuration file present in the repository the action will be triggered from.
+
+> Visit [our getting started guide](https://docs.cloudquery.io/docs/getting-started/getting-started-with-aws/) to learn how to generate a configuration file.
+
+## Setup
+
+Example usage:
+
+Create a `.github/workflows/cloudquery.yml` file with the content of:
+
+```yaml
+name: CloudQuery
+on:
+  schedule:
+    # Run daily at 03:00 (3am)
+    - cron: '0 3 * * *'
+
+jobs:
+  cloudquery:
+    runs-on: ubuntu-latest
+    steps:
+      # Download the repository content with the `config.hcl` file
+      - uses: actions/checkout@v3
+
+      # Setup AWS credentials (example)
+      - name: Configure AWS credentials
+        uses: aws-actions/configure-aws-credentials@v1
+        with:
+          role-to-assume: <role-arn>
+          aws-region: <region>
+
+      - uses: cloudquery/setup-cloudquery@v1
+        name: Setup CloudQuery
+        with:
+          # optional, defaults to latest. Must be a valid SemVer version (e.g. v0.22.9) or latest
+          version: latest
+
+      - name: Fetch with CloudQuery
+        run: cloudquery fetch --config config.hcl
+
+      # Upload logs as a GitHub actions artifact
+      - uses: actions/upload-artifact@v3
+        if: always()
+        with:
+          name: 'cloudquery.log'
+          path: 'cloudquery.log'
+```
---
+++ src/main.ts
@@ -0,0 +1,47 @@
+import * as core from '@actions/core';
+import chalk from 'chalk';
+import { platform } from 'os';
+import { execaCommand } from 'execa';
+import ora from 'ora';
+import semver from 'semver';
+import path from 'path';
+
+const binaries = {
+  darwin: 'cloudquery_darwin_x86_64',
+  linux: 'cloudquery_linux_x86_64',
+};
+
+export const installBinary = async (version: string) => {
+  const binary = binaries[platform() as keyof typeof binaries];
+  if (!binary) {
+    throw new Error(`Unsupported platform: ${platform()}`);
+  }
+  const isLatest = version === 'latest';
+  const message = isLatest ? `${chalk.green('latest')} version` : `version '${chalk.green(version)}'`;
+  const spinner = ora(`Downloading ${message} of CloudQuery`).start();
+  const downloadUrl = isLatest
+    ? `https://github.com/cloudquery/cloudquery/releases/${version}/download/${binary}`
+    : `https://github.com/cloudquery/cloudquery/releases/download/${version}/${binary}`;
+  await execaCommand(`curl -L ${downloadUrl} -o cloudquery`, {
+    stdout: 'inherit',
+  });
+  await execaCommand('chmod +x cloudquery');
+  core.addPath(path.resolve('./'));
+  spinner.succeed(`Finished downloading ${message} of CloudQuery`);
+};
+
+async function main() {
+  try {
+    const version = core.getInput('version', { required: false });
+    if (version !== 'latest' && !semver.valid(version)) {
+      throw new Error(`Invalid version: ${version}`);
+    }
+    await installBinary(version);
+  } catch (err) {
+    const error = err as Error;
+    core.error(error);
+    core.setFailed(error.message);
+  }
+}
+
+main();
