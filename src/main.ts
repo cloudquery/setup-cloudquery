6f68b77bdd74a44a90e9b8910ad46e856fcc07f5
Yevgeny Pats <yev.pats@gmail.com>
2022-06-16 16:17:32 +0300

    feat: Action should only Download CloudQuery
    This will also add it to path so future steps
    or jobs will be able to invole cloudquery without
    full path.

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,42 @@
 import * as core from '@actions/core';
 import chalk from 'chalk';
+import { platform } from 'os';
+import { execaCommand } from 'execa';
+import ora from 'ora';
+import semver from 'semver';
 
-import { getConfig } from './config.js';
-import { installBinary } from './install.js';
-import { fetch as runFetch } from './cloudquery.js';
+const binaries = {
+  darwin: 'cloudquery_darwin_x86_64',
+  win32: 'cloudquery_windows_x86_64.exe',
+  linux: 'cloudquery_linux_x86_64',
+};
+
+export const installBinary = async (version: string) => {
+  const binary = binaries[platform() as keyof typeof binaries];
+  if (!binary) {
+    throw new Error(`Unsupported platform: ${platform()}`);
+  }
+  const isLatest = version === 'latest';
+  const message = isLatest ? `${chalk.green('latest')} version` : `version '${chalk.green(version)}'`;
+  const spinner = ora(`Downloading ${message} of CloudQuery`).start();
+  const downloadUrl = isLatest
+    ? `https://github.com/cloudquery/cloudquery/releases/${version}/download/${binary}`
+    : `https://github.com/cloudquery/cloudquery/releases/download/${version}/${binary}`;
+  await execaCommand(`curl -L ${downloadUrl} -o cloudquery`, {
+    stdout: 'inherit',
+  });
+  await execaCommand('chmod +x cloudquery');
+  core.addPath('./cloudquery');
+  spinner.succeed(`Finished downloading ${message} of CloudQuery`);
+};
 
 async function main() {
   try {
-    core.info('Extracting inputs');
-    const { version, additionalFlags } = await getConfig();
-
-    core.info(`Installing version ${chalk.magenta(version)}`);
+    const version = core.getInput('version', { required: false });
+    if (version !== 'latest' && !semver.valid(version)) {
+      throw new Error(`Invalid version: ${version}`);
+    }
     await installBinary(version);
-
-    return await runFetch(additionalFlags);
   } catch (err) {
     const error = err as Error;
     core.error(error);
