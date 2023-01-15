53778407dc36a2ce642f5da8d2fda12045ca88cc
erezrokah <erezrokah@users.noreply.github.com>
2023-01-15 20:09:29 +0200

    test: Try with homebrew

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,19 @@
 import * as core from '@actions/core';
-import chalk from 'chalk';
 import { platform } from 'os';
 import { execaCommand } from 'execa';
-import ora from 'ora';
 import semver from 'semver';
-import path from 'path';
 
 const binaries = {
-  darwin: 'cloudquery_darwin_amd64',
-  linux: 'cloudquery_linux_amd64',
+  darwin: 'brew install cloudquery/tap/cloudquery',
+  linux: 'brew install cloudquery/tap/cloudquery',
 };
 
-const resolveDownloadUrl = async (version: string, binary: string) => {
-  const tag = version.startsWith('v') ? `cli-${version}` : `cli-v${version}`;
-  return `https://github.com/cloudquery/cloudquery/releases/download/${tag}/${binary}`;
-};
-
-export const installBinary = async (version: string) => {
+export const installBinary = async () => {
   const binary = binaries[platform() as keyof typeof binaries];
   if (!binary) {
     throw new Error(`Unsupported platform: ${platform()}`);
   }
-  const message = `version '${chalk.green(version)}'`;
-  const spinner = ora(`Downloading ${message} of CloudQuery`).start();
-  const downloadUrl = await resolveDownloadUrl(version, binary);
-  await execaCommand(`curl -L ${downloadUrl} -o cloudquery`, {
-    stdout: 'inherit',
-  });
-  await execaCommand('chmod +x cloudquery');
-  core.addPath(path.resolve('./'));
-  spinner.succeed(`Finished downloading ${message} of CloudQuery`);
+  await execaCommand(binary);
 };
 
 async function main() {
@@ -38,7 +22,7 @@ async function main() {
     if (!semver.valid(version)) {
       throw new Error(`Invalid version: ${version}`);
     }
-    await installBinary(version);
+    await installBinary();
   } catch (err) {
     const error = err as Error;
     core.error(error);
